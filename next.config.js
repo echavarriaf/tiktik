7ac5bf9815e91ce06fa66bc29757613fe1e91c2f
Felix Echavarria <echavarria.f@gmail.com>
2022-07-29 10:55:38 -0400

    Initial commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "cdn.pixabay.com",
+      "file-uploads.teachablecdn.com",
+      "p16-amd-va.tiktokcdn.com",
+      "image.shutterstock.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
